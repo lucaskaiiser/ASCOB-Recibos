c7651247ea0b45beee4c384dfb5c7d33dee66d47
lucaskaiiser <lucaskaiiser@live.com>
2025-06-07 12:49:20 -0400

    ignore

--- src/windows/print.py
+++ src/windows/print.py
@@ -1,7 +1,7 @@
 from .main import ttk, tk
 
 class ReceiptPrint(tk.Toplevel):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, receipt_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         
