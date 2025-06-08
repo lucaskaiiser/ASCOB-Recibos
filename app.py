817b54c0508205edc0e40fe7993706141c063f30
lucaskaiiser <lucaskaiiser@live.com>
2025-06-08 17:41:53 -0400

    runtime dlls for windows

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+dll_path = os.path.join(base_dir, 'runtime')
+os.environ['PATH'] = dll_path + os.pathsep + os.environ['PATH']
+
 from src.models import start_database_connection
 from src.windows import MainWindow
 from src.managers.window_manager import WindowManager
